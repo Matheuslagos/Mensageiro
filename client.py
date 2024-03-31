057bfeae2396715371a34a376cc60fc98b59b48f
matheus lagos <matheuslagos@hotmail.com.br>
2024-03-30 21:33:18 -0300

    alteracoes

--- client.py
+++ client.py
@@ -1,22 +1,30 @@
 import socket
+import threading
 
-#SOCK_STREAM = TCP
-#SOCK_DGRAM = UDP
-#AF_INET = IP E PORTA. MODELO INET
-
-HOST = 'localhost'
+HOST = '26.197.241.123'
 PORT = 5007
+
+def send_message(s):
+    while True:
+        mensagem = input('escreva sua mensagem: ')
+        mensagem_em_byte = str.encode(mensagem)
+        s.sendall(mensagem_em_byte)
+
+def receive_messages(s):
+    while True:
+        data = s.recv(1024)
+        print('Received', repr(data))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-while(True):
+send_thread = threading.Thread(target=send_message, args=(s,))
+receive_thread = threading.Thread(target=receive_messages, args=(s,))
 
-    mensagem = input('escreva sua mensagem: ')
-    mensagem_em_byte = str.encode(mensagem)
-    s.sendall(mensagem_em_byte)
-    
-    data = s.recv(1024)
-    print('Received', repr(data))
+send_thread.start()
+receive_thread.start()
+
+send_thread.join()
+receive_thread.join()
 
 s.close()
--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 import socket
+import threading
 
-#SOCK_STREAM = TCP
-#SOCK_DGRAM = UDP
-#AF_INET = IP E PORTA. MODELO INET
-
-HOST = 'localhost'
+HOST = '26.197.241.123'
 PORT = 5007
+
+# Lista para armazenar todas as conexões ativas
+connections = []
+
+def handle_client(conn, addr):
+    print('Conectado a', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print('Recebido de', addr, ':', data.decode())
+        # Repassar a mensagem recebida para todos os clientes, exceto o remetente
+        for connection in connections:
+            if connection != conn:
+                connection.sendall(data)
+    conn.close()
+    connections.remove(conn)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('conectado a', addr)
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            if not data: break
-            conn.sendall(data)
+    s.listen()
+
+    while True:
+        conn, addr = s.accept()
+        # Adicionar nova conexão à lista de conexões ativas
+        connections.append(conn)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
